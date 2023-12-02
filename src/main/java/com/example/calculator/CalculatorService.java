e90ab163181f221fd92dcff39ba1481ffe8b511f
VoreXxx <vorex.00@mail.ru>
2023-12-02 16:59:08 +0600

    Homework done

---
+++ src/main/java/com/example/calculator/CalculatorService.java
@@ -0,0 +1,10 @@
+package com.example.calculator;
+
+public interface CalculatorService {
+    Integer add(Integer num1, Integer num2);
+
+    Integer minus(Integer num1, Integer num2);
+    Integer multiply(Integer num1, Integer num2);
+    Integer divide(Integer num1, Integer num2);
+
+}
